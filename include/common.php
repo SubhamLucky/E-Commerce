82bb807500ce6a371dcf2731732e44248846f415
SubhamLucky <55046842+SubhamLucky@users.noreply.github.com>
2020-06-11 21:24:45 +0530

    update common.php

--- include/common.php
+++ include/common.php
@@ -1,5 +1,6 @@
 <?php
-$con=mysqli_connect("localhost:3308","root","root","Lifestyle") or die(mysqli_error($con));
+$con=mysqli_connect("alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","ypfti26cz37rj8oh","xij35fydc709ln11	
+","nh6yx8p859f4v04q") or die(mysqli_error($con));
 if(!isset($_SESSION)) 
     { 
         session_start(); 
